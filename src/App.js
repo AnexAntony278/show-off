fe5b523ddb2120c5bb824e89b1d72bd347e0d16e
AnexAntony <anexantony278@gmail.com>
2025-04-09 19:01:58 +0530

    reatart port folio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
-import {HeroPage} from './Components/HeroPage'
-import {AboutPage} from './Components/AboutPage'
-import {ContactPage} from './Components/ContactPage'
-import {ControlBar} from './Components/ControlBar'
+import { HeroPage } from './Components/HeroPage'
+import { AboutPage } from './Components/AboutPage'
+import { ContactPage } from './Components/ContactPage'
+import { ControlBar } from './Components/ControlBar'
 import { Cursor } from "./Components/Cursor";
 import { ColorSchemes } from "./Themes";
+import '../src/';
 
-export const App=()=>{
-    const [theme,setTheme]=useState({lightMode:false,colorScheme:0});
-    
+export const App = () => {
+    const [theme, setTheme] = useState({ lightMode: false, colorScheme: 0 });
+
     //Change theme variable in CSS root variables;
-    const rootEl=document.querySelector(":root");
+    const rootEl = document.querySelector(":root");
     rootEl.style.setProperty("--primaryColor", `${ColorSchemes.primaryOf(theme)}`);
     rootEl.style.setProperty("--secondaryColor", `${ColorSchemes.secondaryOf(theme)}`);
     rootEl.style.setProperty("--accentColor", `${ColorSchemes.accentOf(theme)}`);
 
-    return(
+    return (
         <div>
-            <Cursor/>
-            <ControlBar theme={theme} setTheme={setTheme}/>
-            <HeroPage theme={theme}/>
-            <AboutPage theme={theme}/>
-            <ContactPage theme={theme}/>
+            <Cursor />
+            <ControlBar theme={theme} setTheme={setTheme} />
+            <HeroPage theme={theme} />
+            <AboutPage theme={theme} />
+            <ContactPage theme={theme} />
         </div>
-        )
-    
+    )
+
 }
